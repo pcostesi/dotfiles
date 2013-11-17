3dc10b3602d190b18adc676577bd23c0579c8e12
Pablo Costesich <pcostesich@monits.com>
2013-11-17 20:38:32 -0300

    Adds a better temp promp

--- .bashrc.d/90-prompt.sh
+++ .bashrc.d/90-prompt.sh
@@ -1,3 +1,3 @@
 
-PS1="$RED\$(get_ret_code)$LIGHT_GRAY\u@\h $LIGHT_GREEN\$(date '+%H:%M (%d/%m/%Y)') [\$(parse_core_temp)] \n\
+PS1="$RED\$(get_ret_code)$LIGHT_GRAY\u@\h $LIGHT_GREEN\$(date '+%H:%M (%d/%m/%Y)') $BLUE\$(parse_core_temp) \n\
 \w$YELLOW \$(parse_git_branch)$LIGHT_GREEN\$ $LIGHT_GRAY"
