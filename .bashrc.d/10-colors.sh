0b1e420eceba5710cb2deabebaba1d5f7de5a54b
Pablo Alejandro Costesich <rhfixer@gmail.com>
2013-11-25 12:23:01 -0300

    Adds better dircolor support

--- .bashrc.d/10-colors.sh
+++ .bashrc.d/10-colors.sh
@@ -3,4 +3,4 @@ YELLOW="\[\033[0;33m\]"
 GREEN="\[\033[0;32m\]"
 LIGHT_GREEN="\[\033[1;32m\]"
 LIGHT_GRAY="\[\033[0;37m\]"
-eval `dircolors ~/.solarized/dircolors/dircolors.256dark`
+eval `dircolors ~/.solarized/dircolors/dircolors.ansi-dark`
--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ for file in .*; do
 
     # back-up existing symlinks
     if [[ -L $replaces ]]; then
-        cp $replaces $replaces-old
+        cp -r $replaces $replaces-old
     fi
     for avoid in "." ".." ".git"; do
         if [[ $base = $avoid ]]; then
@@ -29,7 +29,11 @@ sudo apt-get install dconf-cli ipython vim git-core
 sudo apt-get install ttf-inconsolata
 bash gnome-terminal-colors-solarized/install.sh
 
-python .vim/bundle/powerline/setup.py install --user
+
+CURDIR=$(pwd)
+cd .vim/bundle/powerline
+python setup.py install --user
+cd $CURDIR
 
 mkdir -p ~/.fonts
 mkdir -p ~/.config/fontconfig/conf.d/
@@ -37,10 +41,10 @@ cp .vim/bundle/powerline/font/*.otf ~/.fonts/
 cp .vim/bundle/powerline/font/*.conf ~/.config/fontconfig/conf.d/
 fc-cache -vf ~/.fonts
 
-cd vim/csv.vim/ && make ; cd -
+cd vim/csv.vim/ && make ; cd $CURDIR
 
 mkdir -p ~/.solarized/dircolors
-cp dircolors-solarized/dircolors.256dark ~/.solarized/dircolors/
+cp dircolors-solarized/dircolors.ansi-dark ~/.solarized/dircolors/
 
 mkdir -p ~/.tmux.d/
 cp .vim/bundle/powerline/powerline/bindings/tmux/powerline.conf ~/.tmux.d/
